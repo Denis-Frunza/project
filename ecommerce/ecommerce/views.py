c5c2bd393d61f05c021e6804c7d53f7bf58d7539
DF TCSS <dftcss@omnigon.com>
2020-01-13 05:59:36 +0300

    add ability to post comments to the product.Also created class(ProductView) to use two different class based views from the same URL(https://docs.djangoproject.com/en/dev/topics/class-based-views/mixins/#using-formmixin-with-detailview)

--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.views.generic import View, TemplateView, ListView, DetailView, FormView, UpdateView, CreateView
 from django.views.generic.edit import FormMixin
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.detail import SingleObjectMixin
+from django.utils import timezone
 
 from . import models
 from . import forms
@@ -11,8 +13,36 @@ class ProductListView(ListView):
     template_name = "ecommerce/shop_product_col_3.html"
 
 
-class SingleProductView(FormMixin, DetailView):
+class SingleProductView(DetailView):
     model = models.Product
-    template_name = "ecommerce/shop_single_product.html"
+    template_name = 'ecommerce/shop_single_product.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = forms.TestimonialForm()
+        context['reviews'] = models.Review.objects.filter(pk=self.kwargs.get('pk'))
+        return context
+
+    #model.objects.create(author=me, title='Sample title', text='Test')
+
+
+class Testimonial(SingleObjectMixin, FormView):
+    template_name = 'ecommerce/shop_single_product.html'
     form_class = forms.TestimonialForm
+    model = models.Product
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+
+class ProductDetail(View):
+
+    def get(self, request, *args, **kwargs):
+        view = SingleProductView.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = Testimonial.as_view()
+        return view(request, *args, **kwargs)
 
